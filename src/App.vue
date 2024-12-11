b7c9d3db06b72ee0f389b5d955a03ddf20e0bc6e
Leandro Agustin Meier <leannmeier72@gmail.com>
2024-12-10 21:06:59 -0300

    PNT2-Final 10/12/2024

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container mt-3">
+    <div class="jumbotron">
+      <h1>PNT2 - Final 10/12/2024</h1>
+      <h2>Alumno: Meier Leandro Agustin</h2>
+      <br>
+      <Textos />
+      <hr>
+      <Respuestas />
+    </div>
+  </div>
+</template>
+
+<script>
+import Textos from './componentes/Textos.vue'
+import Respuestas from './componentes/Respuestas.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Textos,
+    Respuestas,
+  }
+}
+</script>
+
+<style scoped lang="css">
+  h1, h2{
+    color: crimson;
+  }
+  .jumbotron {
+    padding-top: 40px;
+    padding-bottom: 40px;
+  }
+  hr{
+    background-color: #000;
+  }
+</style>
---
+++ src/componentes/Textos.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="jumbotron">
+    <form @submit.prevent>
+      <input type="text" v-model="textoIngresado" placeholder="Ingrese un texto"/>
+    </form>
+    <div v-show="visibilidad()" >
+      <p>1.- Cantidad de Caracteres: {{ cantCaracteres }}</p>
+      <p>2.- Cantidad de Vocales: {{ cantVocales }}</p>
+      <p>3.- Texto en Mayúsculas: {{ textoMayuscula }}</p>
+      <p>4.- Texto Invertido: {{ textoInvertido }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    textoIngresado: '',
+    }
+  },
+  computed: {
+    cantCaracteres() {
+      return this.textoIngresado.length;
+    },
+    cantVocales() {
+      let vocales = 0;
+      for (let char of this.textoIngresado.toLowerCase()) {
+        if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+          vocales++;
+        }
+      }
+      return vocales;
+    },
+    textoMayuscula() {
+      return this.textoIngresado.toUpperCase();
+    },
+    textoInvertido() {
+      let otxet = '';
+      for (let i = this.textoIngresado.length - 1; i >= 0; i--) {
+        otxet += this.textoIngresado[i];
+      }
+      return otxet;
+    },
+  },
+  methods: {
+    visibilidad(){
+      return this.textoIngresado.length
+    }
+  }
+}
+</script>
+
+<style scoped lang="css">
+  .jumbotron{
+    font-weight: bold;
+  }
+</style>
